3ffc6b74fb062d2a70b831fa45ec975504af9e65
Kartik Shandilya <kartikdroid17@gmail.com>
2018-03-03 19:01:25 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/ojasvi/justjava/MainActivity.java
+++ app/src/main/java/com/example/ojasvi/justjava/MainActivity.java
@@ -8,27 +8,31 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    public Button but1;
-    public void init(){
-        but1= (Button)findViewById(R.id.but1);
-        but1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    public Button button;
 
-                Intent toy = new Intent(MainActivity.this,Main2Activity.class);
-
-                startActivity(toy);
-            }
-        });
-
-    }
-
-    @Override
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
     }
+
+    
+    
+    public void init(){
+        button = findViewById(R.id.but1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+
+                startActivity(intent);
+            }
+        });
+
+    }
+
 
 
 }
